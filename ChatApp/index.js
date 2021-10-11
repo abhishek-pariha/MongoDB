10660b58e20351cd359bece381372517c8a4d757
abhishek-pariha <88427546+abhishek-pariha@users.noreply.github.com>
2021-10-11 18:55:12 +0530

    chatapp

--- ChatApp/index.js
+++ ChatApp/index.js
@@ -1,41 +1,26 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const {Server} = require('socket.io');
-const io = new Server(server);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
-
-  io.on('connection',(socket) => {
-      console.log("a user connected");
-  })
-
-  io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
-
-  io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-    });
-  });
-
-  io.on('connection', (socket) => {
-    socket.broadcast.emit('hi');
-  });
-
-  io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-  });
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+app.get('/', function(req, res){
+   res.sendFile('/home/webcodegenie/Documents/adminlte/MongoDB/ChatApp/index.html');});
+users = [];
+io.on('connection', function(socket){
+   console.log('A user connected');
+   socket.on('setUsername', function(data){
+      console.log(data);
+      if(users.indexOf(data) > -1){
+         socket.emit('userExists', data + ' username is taken! Try some other username.');
+      } else {
+         users.push(data);
+         socket.emit('userSet', {username: data});
+      }
+   });
+   socket.on('msg', function(data){
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+   })
+});
+http.listen(3000, function(){
+   console.log('listening on localhost:3000');
 });
